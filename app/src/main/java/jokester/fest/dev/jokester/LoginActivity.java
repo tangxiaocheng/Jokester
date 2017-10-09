585ee32386763035f3d37ba4802130faadece9bf
nelson <pleasespammelater@gmail.com>
2017-10-09 16:45:49 -0400

    Continued working on login using Google Auth

--- app/src/main/java/jokester/fest/dev/jokester/LoginActivity.java
+++ app/src/main/java/jokester/fest/dev/jokester/LoginActivity.java
@@ -2,6 +2,7 @@ package jokester.fest.dev.jokester;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -9,9 +10,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1001;
+    private GoogleApiClient mGoogleApiClient;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +31,39 @@ public class LoginActivity extends AppCompatActivity {
         cmdGoogleSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(LoginActivity.this, "Logging in ...NOT!",Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(myIntent);
+                SignInFake();
+//                SignInReal();
             }
         });
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleApiClient.OnConnectionFailedListener ocfl = new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(LoginActivity.this, "Google connection failure",Toast.LENGTH_LONG).show();
+
+            }
+        };
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, ocfl /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+    }
+
+    private void SignInReal() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    private void SignInFake() {
+        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(myIntent);
     }
 
 }
