5d567fbf82c14970a43b81c3955bde31c1dc4b22
Dengcheng <tangdengcheng@gmail.com>
2017-10-09 16:53:15 -0400

    add proguard

--- app/src/main/java/jokester/fest/dev/jokester/fragments/FragmentOne.java
+++ app/src/main/java/jokester/fest/dev/jokester/fragments/FragmentOne.java
@@ -12,6 +12,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import jokester.fest.dev.jokester.R;
 
 /**
@@ -20,6 +25,8 @@ import jokester.fest.dev.jokester.R;
 public class FragmentOne extends Fragment {
 
     EditText m_txtJokeText;
+    private FirebaseDatabase dref;
+    private DatabaseReference dataRef;
 
     @Nullable
     @Override
@@ -32,6 +39,9 @@ public class FragmentOne extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        dref = FirebaseDatabase.getInstance();
+        dataRef = dref.getReference("FirebaseAppJokes/jokesByList");
         m_txtJokeText= (EditText) view.findViewById(R.id.txtNewJoke);
 //        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 //        imm.showSoftInput(m_txtJokeText, InputMethodManager.SHOW_IMPLICIT);
@@ -40,10 +50,31 @@ public class FragmentOne extends Fragment {
         btnAddJoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+            String content =    m_txtJokeText.getText().toString();
+
+//              Auth auth =  FirebaseAuth.getInstance();
+
+                JokeModel model = new JokeModel();
+                model.setBody(content);
+                model.setTitle(content);
+//                model.setTitle(dref.getApp().g);
+
+
                 Toast.makeText(getActivity(), "Your joke has been added ... NOT!",Toast.LENGTH_LONG).show();
                 m_txtJokeText.setText("");
+
+
                 InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 imgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+
+
+
+
+                DatabaseReference newPostRef = dataRef.push();
+                newPostRef.setValue(model);
+
 
             }
         });
