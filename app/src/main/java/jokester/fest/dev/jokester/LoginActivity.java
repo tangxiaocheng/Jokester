54d75d6727c75e89076ec34fb4aecfcc49123c56
nelson <pleasespammelater@gmail.com>
2017-10-09 16:22:43 -0400

    continued working on login activity

--- app/src/main/java/jokester/fest/dev/jokester/LoginActivity.java
+++ app/src/main/java/jokester/fest/dev/jokester/LoginActivity.java
@@ -1,7 +1,14 @@
 package jokester.fest.dev.jokester;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignInButton cmdGoogleSignIn = (SignInButton) findViewById(R.id.cmdGoogleSignIn);
+        cmdGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Logging in ...NOT!",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
+
 }
